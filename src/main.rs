7d486c4795ee4b6d97271d31ac4f33d71252b0b8
Ilya Tkemaladze <intkemaladze@gmail.com>
2025-01-16 18:30:09 +0300

    simple example

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+fn main() {
+    let p = Person{name: "men".to_string(), age: 32, phones: vec!["7-909-...".to_string(), "8-925-...".to_string()] };
+
+    println!("{}", serde_json::to_string_pretty(&p).unwrap());
+}
