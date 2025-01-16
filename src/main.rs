1bf92acdd75a6150f4916f6e5e72627b5018da36
Ilya Tkemaladze <intkemaladze@gmail.com>
2025-01-16 19:56:14 +0300

    bad format

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ struct Person {
 }
 fn main() {
     let p = Person {
-        name: "men".to_string(),
-        age: 24,
+        name: "men".to_string(), age: 24,
         phones: vec!["7-909-...".to_string(), "8-925-...".to_string()],
     };
 
